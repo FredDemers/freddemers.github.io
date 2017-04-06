bdee00904005d7c8a323c2d1bb91495ff79b064d
FredDemers <fdemers@shaw.ca>
2017-04-06 11:29:39 -0500

    purple rain ayyyy

--- port/drop.js
+++ port/drop.js
@@ -26,7 +26,7 @@ function Drop() {
   this.show = function() {
     var thick = map(this.z, 0, 20, 1, 3);
     strokeWeight(thick);
-    stroke(191, 238, 255);
+    stroke(87, 80, 150);
     line(this.x, this.y, this.x, this.y+this.len);
   }
 }
