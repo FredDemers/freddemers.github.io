99a912d2a141a1bda28be3252ff60c92c6d6881a
FredDemers <fdemers@shaw.ca>
2017-10-11 09:09:42 -0500

    vertex

--- toto/sketch.js
+++ toto/sketch.js
@@ -1,25 +1,25 @@
-document.ontouchmove = function(event){
- event.preventDefault();
+document.ontouchmove = function(event) {
+  event.preventDefault();
 }
 
 function setup() {
- createCanvas(windowWidth,windowHeight);
- ellipseMode(CENTER);
- stroke(0);
- noFill();
- strokeWeight(5);
+  createCanvas(windowWidth, windowHeight);
+  ellipseMode(CENTER);
+  stroke(0);
+  noFill();
+  strokeWeight(5);
 }
 
-function draw(){
- background(255);
- 
- for(var i=0;i<touches.length;i++){
-  ellipse(touches[i].x,touches[i].y,150,150);
-  }
+function draw() {
+  background(255);
+
+  for (var i = 0; i < touches.length; i++) {
+    ellipse(touches[i].x, touches[i].y, 150, 150);
   beginShape();
-  vertex(touches[1],touches[2]);
-   vertex(touches[2],touches[3]);
-    vertex(touches[3],touches[4]);
-     vertex(touches[4],touches[5]);
-     endShape();
+  vertex(touches[1], touches[2]);
+  vertex(touches[2], touches[3]);
+  vertex(touches[3], touches[4]);
+  vertex(touches[4], touches[5]);
+  endShape();}
+  
 }
