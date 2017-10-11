bcf328f2c7ad1127f3f33ad098d6662bbb444533
FredDemers <fdemers@shaw.ca>
2017-10-11 10:20:13 -0500

    end shape close

--- toto/sketch.js
+++ toto/sketch.js
@@ -11,16 +11,19 @@ function setup() {
 }
 
 function draw() {
-  background(155);
+  background(255);
   beginShape();
   for (var i = 0; i < touches.length; i++) {
     vertex(touches[i].x, touches[i].y);
   }
-  endShape();
+  endShape(CLOSE);
   fill(200, 115, 100);
 
 
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, 150, 150);
   }
+/*var posx =map(touches[i].x);
+var posy =map(touches[i].y);
+text(posx,touches[i].x+10);*/
 }
