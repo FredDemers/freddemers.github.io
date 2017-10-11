f33a5d7991d05266c4601329fbabfe22c8ab2730
FredDemers <fdemers@shaw.ca>
2017-10-11 11:20:06 -0500

    add numbers

--- toto/sketch.js
+++ toto/sketch.js
@@ -1,17 +1,25 @@
 document.ontouchmove = function(event) {
-  event.preventDefault();
-}
+    event.preventDefault();
+  }
+  /*var dummy = [];
+  var dummyMax;*/
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  stroke(0);
-  noFill();
-  strokeWeight(5);
+  /*dummyMax = random(1, 4);
+  for (var i = 0; i < dummyMax; i++){
+    dummy[i] = new Object;
+    dummy[i].x=random(0,windowWidth);
+    dummy[i].y=random(0,windowWidth);*/
+
+ellipseMode(CENTER);
+stroke(0);
+noFill();
+strokeWeight(5);
 }
 
 function draw() {
-  background(255);
+  background(0);
   beginShape();
   for (var i = 0; i < touches.length; i++) {
     vertex(touches[i].x, touches[i].y);
@@ -25,7 +33,7 @@ function draw() {
     var posx = floor(touches[i].x);
     var posy = floor(touches[i].y);
     textSize(70);
-    text(posx, touches[i].x+100, touches[i].y-100);
-    text(posy, touches[i].x+100, touches[i].y+100);
+    text(posx, touches[i].x + 75, touches[i].y - 25);
+    text(posy, touches[i].x + 75, touches[i].y + 50);
   }
 }
