f3a62148e0affd6dc7c596ae97b9836026bef8af
FredDemers <fdemers@shaw.ca>
2017-10-11 09:28:50 -0500

    vertex

--- toto/sketch.js
+++ toto/sketch.js
@@ -16,11 +16,7 @@ function draw() {
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, 150, 150);
   beginShape();
-  vertex(touches[1].x, touches[1].y);
-  vertex(touches[2].x, touches[2].y);
-  vertex(touches[3].x, touches[3].y);
-  vertex(touches[4].x, touches[4].y);
-  vertex(touches[5].x, touches[5].y);
+  vertex(touches[i].x, touches[i].y);
   endShape(CLOSE);}
   
 }
