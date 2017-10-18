d978cfedcc9a59d61e504461ae94f9399122b83f
FredDemers <fdemers@shaw.ca>
2017-10-18 09:28:08 -0500

    move

--- rot/sketch.js
+++ rot/sketch.js
@@ -2,14 +2,15 @@ var x=0;
 var y=0;
 var vx=0;
 var vy=0;
-var devrotx, devroty, vmulti, vx, ax, vy, ay;
+var ax=0;
+var ay=0;
+var devrotx, devroty, vmulti;
 
 function setup() {
  createCanvas(windowWidth,windowHeight); 
    x = windowWidth/2;
    y= windowHeight/2;
    vmulti=0.01;
-   ax=
    
    
 }
@@ -23,17 +24,24 @@ function draw() {
  text("Rx: " + rotationX, 100, 100);
  text("Ry: " + rotationY, 100, 150);
  text("Rz: " + rotationZ, 100, 200);
+ rot();
+ move();
  drawEllipse();
 }
  function drawEllipse(){
    ellipse(x,y,150,150);
  }
- 
+ function rot(){
+   devrotx=rotationX;
+   devroty=rotationY;
+ }
  function move(){
    ax=devroty*vmulti;
    ay=devrotx*vmulti;
+    
     vx+=ax;
     vy+=ay;
+   
     x+=vx;
     y+=vy
  }
