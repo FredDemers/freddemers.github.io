29bff27db5866fc5ed5717fc3af9e89296abfbe8
FredDemers <fdemers@shaw.ca>
2017-10-18 08:45:57 -0500

    rot floor

--- rot/sketch.js
+++ rot/sketch.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
  background(255);
  textSize(40);
- text("Rx: " + rotationX, 100, 100);
- text("Ry: " + rotationY, 100, 150);
- text("Rz: " + rotationZ, 100, 200);
+ text(floor("Rx: " + rotationX, 100, 100));
+ text(floor("Ry: " + rotationY, 100, 150));
+ text(floor("Rz: " + rotationZ, 100, 200));
 }
