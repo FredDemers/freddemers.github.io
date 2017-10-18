cf36e309a57e15e5f1e35917d0c49994251f7930
FredDemers <fdemers@shaw.ca>
2017-10-18 08:56:10 -0500

    fill

--- rot/sketch.js
+++ rot/sketch.js
@@ -1,3 +1,8 @@
+var x=0;
+var y=0;
+var vx=0;
+var vy=0;
+
 function setup() {
  createCanvas(windowWidth,windowHeight); 
 }
@@ -5,7 +10,12 @@ function setup() {
 function draw() {
  background(255);
  textSize(40);
+ fill(133,236,112);
  text(floor("Rx: " + rotationX, 100, 100));
  text(floor("Ry: " + rotationY, 100, 150));
  text(floor("Rz: " + rotationZ, 100, 200));
 }
+ function drawEllipse(){
+   ellipse(x,y,150,150);
+ }
+ 
