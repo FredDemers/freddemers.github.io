b2e07e55f1a10b38341f71275a36b616013140d4
FredDemers <fdemers@shaw.ca>
2017-10-18 09:40:48 -0500

    bounce

--- rot/sketch.js
+++ rot/sketch.js
@@ -1,47 +1,62 @@
-var x=0;
-var y=0;
-var vx=0;
-var vy=0;
-var ax=0;
-var ay=0;
+var x = 0;
+var y = 0;
+var vx = 0;
+var vy = 0;
+var ax = 0;
+var ay = 0;
 var devrotx, devroty, vmulti;
 
 function setup() {
- createCanvas(windowWidth,windowHeight); 
-   x = windowWidth/2;
-   y= windowHeight/2;
-   vmulti=0.01;
-   
-   
+  createCanvas(windowWidth, windowHeight);
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+  vmulti = 0.01;
+  bmulti = 0.6;
+
+
 }
 
 function draw() {
-
- background(155);
- textSize(40);
- fill(133,236,112);
- noStroke();
- text("Rx: " + rotationX, 100, 100);
- text("Ry: " + rotationY, 100, 150);
- text("Rz: " + rotationZ, 100, 200);
- rot();
- move();
- drawEllipse();
+  background(155);
+  textSize(40);
+  rot();
+  move();
+  info();
+  drawEllipse();
 }
- function drawEllipse(){
-   ellipse(x,y,150,150);
- }
- function rot(){
-   devrotx=rotationX;
-   devroty=rotationY;
- }
- function move(){
-   ax=devroty*vmulti;
-   ay=devrotx*vmulti;
-    
-    vx+=ax;
-    vy+=ay;
-   
-    x+=vx;
-    y+=vy
- }
+
+function drawEllipse() {
+  fill(133, 236, 112);
+  noStroke();
+  ellipse(x, y, 150, 150);
+}
+
+function rot() {
+  devrotx = rotationX;
+  devroty = rotationY;
+}
+
+function info() {
+  textSize(40);
+  fill(0);
+  text("Rx: " + rotationX, 100, 100);
+  text("Ry: " + rotationY, 100, 150);
+  text("Rz: " + rotationZ, 100, 200);
+}
+
+function move() {
+  ax = devroty * vmulti;
+  ay = devrotx * vmulti;
+
+  vx += ax;
+  vy += ay;
+
+  x += vx;
+  y += vy;
+  
+  if(x<0 || x>windowWidth){
+    vx=-vx*bmulti;
+ if(y<0 || y>windowWidth){
+    vy=-vy*bmulti;
+  }
+}
